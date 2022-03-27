2d9dfefa07620f122da5e510d8115a3df80473ea
Tiffany Clark <TClark@smartts.net>
2022-03-27 17:09:41 -0500

    jsreviewproject files

---
+++ script.js
@@ -0,0 +1,27 @@
+// Start your code ***HERE*** =========
+
+// create an array with all possible character types: Uppercase, Lowercase, numbers, and symbols stored in a variable
+
+// create a global variable called "pwLength" with a number between 10 and 18
+
+// Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
+
+var password=document.getElementById("password");
+
+ function genPassword() {
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const pwLength = (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i);
+    pwlength (10,18);
+
+
+
+// ========= ⬇ DO NOT TOUCH THIS CODE ⬇ ======
+
+let genBtn = document.getElementById("btnGen");
+let buttonHandler = () => {
+    document.getElementById("password").value = addNewPassword();
+};
+// Event listener for generate PW button
+genBtn.addEventListener("click", buttonHandler);
+
+// ========= ⬆ DO NOT TOUCH THIS CODE ⬆ ======
